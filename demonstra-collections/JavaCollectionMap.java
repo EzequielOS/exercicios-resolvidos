3e256532a89e16ec741d020f077aa1dc26ae8b41
Ezequiel Oliveira <ezequiel.santana@sankhya.com.br>
2019-01-30 16:47:02 -0200

    Implementa HashSet na classe JavaCollectionMap.java

--- demonstra-collections/JavaCollectionMap.java
+++ demonstra-collections/JavaCollectionMap.java
@@ -1,21 +1,33 @@
 /*
 Nome do autor: Ezequiel de Oliveira Sant'Ana
-Data de criação do arquivo: 29/01/2019
-Objetivo sucinto do programa: 
-Referência ao enunciado/origem do exercício:https://docs.google.com/document/d/16F6QhldM5D8wA6RISbLAvKviZ6amAqLVrmMzm5lDMLM/edit 
-Informações adicionais: 
+Data de criaÃ§Ã£o do arquivo: 29/01/2019
+Objetivo sucinto do programa:
+ReferÃªncia ao enunciado/origem do exercÃ­cio:https://docs.google.com/document/d/16F6QhldM5D8wA6RISbLAvKviZ6amAqLVrmMzm5lDMLM/edit
+InformaÃ§Ãµes adicionais:
 */
 
 import java.util.HashMap;
 public class JavaCollectionMap {
 	public static void main(String[] args) {
 		HashMap<String, Integer> myBooks = new HashMap<>();
-		
+
 		myBooks.put("Ender's Game", 4);
 		myBooks.put("Ringworld", 3);
 		myBooks.put("Heir to the Empire", 5);
-		
+
 		System.out.println(myBooks.containsValue(5));//true
 		System.out.println(myBooks.containsValue(2));//false
+
+		/*
+		HashSet<String> mySet = new HashSet<>();
+
+		mySet.add("Banana");
+		mySet.add("Apple");
+		mySet.add("Orange");
+
+		for (String fruit : mySet) {
+			System.out.println(fruit);
+		}
+		*/
 	}
 }
